4b26bb087a091c7cfa2f5f2f85c7bbfaa30bc8ac
denis-gr <qwertyuiop852002058@yandex.com>
2023-04-16 14:47:51 +0000

    Hi world

---
+++ prser.cs
@@ -0,0 +1,10 @@
+using System;
+using Parser;
+
+namespace Parser {
+    class TelegramBot {
+        static void Main(string[] args) {
+            Console.WriteLine("Hi world");
+        } 
+    }
+}
